e6246bb4718ef47d65a8a85d3b45a0d21025cf42
mrezkov <mrezkov@phoenixit.ru>
2023-08-09 17:40:03 +0300

    mobile styles main page and header

--- src/js/componetns/header.js
+++ src/js/componetns/header.js
@@ -1,5 +1,7 @@
 const contactsNavItem = document.querySelector('.contacts__title');
 const contactsPopup = document.querySelector('.contacts-popup');
+const burgerBtn = document.querySelector('.navbar__burger');
+const navLinksMobile = document.querySelector('.navbar__nav-links_mobile');
 
 let isContactsPopupOpen = false;
 contactsNavItem.addEventListener('click', (e) => {
@@ -8,6 +10,18 @@ contactsNavItem.addEventListener('click', (e) => {
         ? contactsPopup.classList.remove('contacts-popup_visible')
         : contactsPopup.classList.add('contacts-popup_visible');
         isContactsPopupOpen = !isContactsPopupOpen;
+});
+
+let isBurgerOpen = false;
+burgerBtn.addEventListener('click', (event) => {
+    if (isBurgerOpen) {
+        event.currentTarget.classList.remove('burger-open');
+        navLinksMobile.classList.remove('navbar__nav-links_mobile_visible');
+    } else {
+        event.currentTarget.classList.add('burger-open');
+        navLinksMobile.classList.add('navbar__nav-links_mobile_visible');
+    }
+    isBurgerOpen = !isBurgerOpen;
 });
 
 document.body.addEventListener('click', () => {
